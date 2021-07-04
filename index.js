fe85d9d0d6f49f6bccae6f89a1dee6f78bff0ad3
Mikko <mkarvonen34@gmail.com>
2021-07-04 15:44:30 +0300

    Add ability to update tasks

--- index.js
+++ index.js
@@ -11,13 +11,26 @@ app.get("/", function (req, res) {
 });
 
 app.post("/api/new_task", function (req, res) {
-  console.log(req.body);
   const body = req.body;
   if (body.name && body.points) {
-    tasks.push({ name: body.name, points: body.points });
+    tasks.push({
+      id: new Date().getTime(),
+      name: body.name,
+      points: body.points,
+    });
     res.send("New task added");
   } else {
     res.send("Task wasn't added");
+  }
+});
+
+app.patch("/tasks/:id", function (req, res) {
+  const task = tasks.find((x) => x.id == req.params.id);
+  if (task) {
+    task.lastUpdate = new Date();
+    res.send(`Task ${task.name} updated`);
+  } else {
+    res.send("Task wasn't updated");
   }
 });
 
